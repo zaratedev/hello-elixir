2e53d41fd30e71336500b641f531dd2294c47a25
Jonathan Zarate <zaratedev@gmail.com>
2024-05-08 13:34:43 -0600

    Manage terminete gen server

--- lib/server_calculator.ex
+++ lib/server_calculator.ex
@@ -1,19 +1,33 @@
 defmodule ServerCalculator do
   use GenServer
 
+  def add(pid, x, y) do
+    GenServer.call(pid, {:add, x, y})
+  end
+
   def init(args) do
     IO.puts "Start server calculator"
     IO.inspect args
+    {:ok, %{count: 0}, 3000}
+  end
 
-    # {:ok, %{count: 0}}
-
-    {:stop, "Error connection"}
+  def terminate(_reason, _state) do
+    IO.puts "Game over"
+    nil
   end
 
   def handle_call({:add, x, y}, _from, %{count: count}) do
     IO.puts "handle_call(:add, #{x}, #{y})"
     result = x + y
-    {:reply, result, %{count: count + 1}}
+    {:noreply, result, %{count: count + 1}}
+  end
+
+  def handle_call({:div, x, y}, _from, state) do
+    if y == 0 do
+      {:stop, "Division by zero", state}
+    else
+      {:reply, x / y, state}
+    end
   end
 
   def handle_cast(:reset, _state) do
@@ -21,13 +35,14 @@ defmodule ServerCalculator do
     {:noreply, %{count: 0}}
   end
 
-  def handle_info(msg, %{count: count}) do
-    IO.puts "Message #{msg}"
+  def handle_info(:hello, %{count: count}) do
     IO.puts "Count #{count}"
 
     {:noreply, %{count: count + 1}}
   end
-  def add(pid, x, y) do
-    GenServer.call(pid, {:add, x, y})
+
+  def handle_info(msg, state) do
+    IO.inspect msg, label: "INFO"
+    {:noreply, state}
   end
 end
