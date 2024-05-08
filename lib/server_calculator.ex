e92cb7d9d54fe434d24f249f6c0c292abcdac1b5
Jonathan Zarate <zaratedev@gmail.com>
2024-05-08 11:07:48 -0600

    handle cast & handle call

--- lib/server_calculator.ex
+++ lib/server_calculator.ex
@@ -8,10 +8,24 @@ defmodule ServerCalculator do
     {:ok, %{count: 0}}
   end
 
+  def handle_call({:add, x, y}, _from, %{count: count}) do
+    IO.puts "handle_call(:add, #{x}, #{y})"
+    result = x + y
+    {:reply, result, %{count: count + 1}}
+  end
+
+  def handle_cast(:reset, _state) do
+    IO.puts "handle_cast(:reset)"
+    {:noreply, %{count: 0}}
+  end
+
   def handle_info(msg, %{count: count}) do
     IO.puts "Message #{msg}"
     IO.puts "Count #{count}"
 
     {:noreply, %{count: count + 1}}
+  end
+  def add(pid, x, y) do
+    GenServer.call(pid, {:add, x, y})
   end
 end
